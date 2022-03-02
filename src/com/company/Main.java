3f8978ce299df11bcfa098e9d59e58e17bcee8b1
Airis1k <airingasstar@gmail.com>
2022-03-02 17:28:04 +0200

    Commitas

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Sveikas pasauli!");
+        System.out.println("Sveikas, vartotojau!");
+    }
+}
